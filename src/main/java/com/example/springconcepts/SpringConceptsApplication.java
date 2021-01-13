33003bc424abe297d3ff0ea4e1588d4400068419
ManaliParab12 <manaliparab10@gmail.com>
2021-01-13 16:03:28 +0530

    Added Logger to log Messages using SLF4J- Simple Logging Framework

--- src/main/java/com/example/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/example/springconcepts/SpringConceptsApplication.java
@@ -1,13 +1,21 @@
 package com.example.springconcepts;
 
+import com.example.springconcepts.component.DemoBean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to spring concept Demo");
-		SpringApplication.run(SpringConceptsApplication.class, args);
+		logger.debug("Welcome to spring concept Demo");
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		logger.debug("Checking Context: {}",context.getBean(DemoBean.class));
+		logger.debug("\n*** Example Using @Autowire annotation on property ***");
 	}
 }
